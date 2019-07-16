a162e4545ea8ab37cea29a298614e26edcc6990b
hwi <khdrogba@gmail.com>
2019-07-16 16:30:21 +0900

    get *

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(loginRoutes);
 //static
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendfile(path.join(__dirname = 'client/build/index.html'));
     })
 } 

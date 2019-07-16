a89f9f61627e4543d96c33eef1ab557db8ca6eeb
hwi <khdrogba@gmail.com>
2019-07-16 15:24:01 +0900

    error

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path')
 const PORT = process.env.PORT || 5000;
 const mongoConnect = require('./utils/db').mongoConnect;
 const session = require('express-session')
